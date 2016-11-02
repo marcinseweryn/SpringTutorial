3c53adf5f5104dad2f94f3a6d72b15ac91940c1a
marcinseweryn <marcin-seweryn@wp.pl>
2016-11-02 01:04:18 +0100

    Module 6, Aspects, part 2

--- Module6/src/main/java/aspect/SimpleAspect.java
+++ Module6/src/main/java/aspect/SimpleAspect.java
@@ -6,31 +6,39 @@ import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class SimpleAspect {
-	
-	@Before(value = "bean(*)")
-	public void befor(JoinPoint joinPoint){
-		
-		Signature signature = joinPoint.getSignature();
-		String name = signature.getName();
-		
-		System.out.println("Przed wywo³aniem metody "+name);
-	}
-	
-	@Around(value = "bean(*)")
-	public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
-		
-		System.out.println("przed");
-		
-		Object proceed = proceedingJoinPoint.proceed();
 
-		System.out.println("po");
-		
-		return proceed;
+	@Before(value = "allBeans() && args(subject,startDate,closeDate)", argNames = "subject,startDate,closeDate")
+	public void befor(String subject, String startDate, String closeDate) {
+
+		//Signature signature = joinPoint.getSignature();
+		//String name = signature.getName();
+
+		/*
+		 * Object[] args = joinPoint.getArgs(); for (int i = 0; i < args.length;
+		 * i++) { System.out.println("Parametr " + args[i]); }
+		 */
+
+		System.out.println("Subject " + subject);
+		System.out.println("Start " + startDate);
+		System.out.println("Close " + closeDate);
+
+		//System.out.println("Przed wywo³aniem metody " + name);
 	}
-	
+
+	@Pointcut(value = "bean(*)")
+	public void allBeans() {
+
+	}
+
+	@Pointcut(value = "within(*..*Service)")
+	public void onlyServices() {
+
+	}
+
 }
