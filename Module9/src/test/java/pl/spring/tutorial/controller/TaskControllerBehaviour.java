5536732445ec7bcde6bcce2562b4f131e273a8da
marcinseweryn <marcin-seweryn@wp.pl>
2016-11-11 14:27:22 +0100

    Module 9, Tests, part 2

--- Module9/src/test/java/pl/spring/tutorial/controller/TaskControllerBehaviour.java
+++ Module9/src/test/java/pl/spring/tutorial/controller/TaskControllerBehaviour.java
@@ -1,21 +1,44 @@
 package pl.spring.tutorial.controller;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.ModelAndViewAssert.*;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
+
+
+
+import pl.spring.tutorial.model.Task;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(
-		{"file:src/main/WEB-INF/spring/appServlet/servlet-context.xml",
-		"file:src/main/WEB-INF/spring/root-context.xml"})
+		{"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
 public class TaskControllerBehaviour {
 
-	@Test
-	public void should_return_task_list_and_task_form_command() {
+	@Autowired
+	TaskController controler;
 	
+	@Test
+	public void should_return_task_list_and_task_form_command() throws Exception {
+		MockHttpServletRequest req = new MockHttpServletRequest("GET","/all-tasks");
+		
+		AnnotationMethodHandlerAdapter adapter = new AnnotationMethodHandlerAdapter();
+		
+		ModelAndView handle = adapter.handle(req,new MockHttpServletResponse(),controler);
+		
+		assertAndReturnModelAttributeOfType(handle, "allTasks",List.class);
+		assertAndReturnModelAttributeOfType(handle, "task",Task.class);
+		
 	}
 
 }
